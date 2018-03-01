ca3b01462a7fd93a9a9f8cdabafedcc86a0b09a9
zhujiajia <772978409@qq.com>
2018-03-01 09:31:25 +0800

    初始化项目

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: ['./src/index.js'],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        port: 18357,
+        historyApiFallback: true,
+    },
+    devtool: 'source-map', // source-map
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+            }
+        }, ]
+    }
+}
