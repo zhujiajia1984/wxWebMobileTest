3a432bc029cc65e2ee01c59f990e478855d9840b
zhujiajia <772978409@qq.com>
2018-03-12 08:47:55 +0800

    testes6

--- src/App.js
+++ src/App.js
@@ -11,153 +11,151 @@ import './styles/myStyles.less';
 
 //
 export default class App extends React.Component {
-	//
-	constructor(props) {
-		super(props);
-		this.test();
-		// console.log(firstName);
-		// console.log(lastName);
-		// console.log(getFirstName());
-		// console.log(test.firstName);
-		// console.log(test.lastName);
-		// console.log(test.getFirstName());
-		// console.log(getName());
-	}
+    //
+    constructor(props) {
+        super(props);
+        this.test();
+        // console.log(firstName);
+        // console.log(lastName);
+        // console.log(getFirstName());
+        // console.log(test.firstName);
+        // console.log(test.lastName);
+        // console.log(test.getFirstName());
+        // console.log(getName());
+    }
 
-	//
-	test() {
+    //
+    test() {}
 
-	}
+    // 配置jssdk config
+    async setConfig() {
+        return await this.getSignInfo();
+    }
 
-	// 配置jssdk config
-	async setConfig() {
-		return await this.getSignInfo();
-	}
+    // 配置jssdk
+    componentDidMount() {
+        this.setConfig().then(signData => {
+            // console.log(signData);
+            wx.config({
+                debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                appId: 'wxaed97ec85f7517ba', // 必填，公众号的唯一标识
+                timestamp: signData.timestamp, // 必填，生成签名的时间戳
+                nonceStr: signData.noncestr, // 必填，生成签名的随机串
+                signature: signData.signature, // 必填，签名
+                jsApiList: ['chooseImage', 'onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表
+            });
+            // jssdk ready / error
+            wx.ready(function() {
+                console.log('success');
+                wx.onMenuShareAppMessage({
+                    title: '123', // 分享标题
+                    desc: 'abc', // 分享描述
+                    link: 'https://test.weiquaninfo.cn/wxWebMobileTest', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                    imgUrl: 'https://test.weiquaninfo.cn/images/baidu.jpg', // 分享图标
+                    type: 'link', // 分享类型,music、video或link，不填默认为link
+                    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                    success: function() {
+                        // 用户确认分享后执行的回调函数
+                        console.log('onMenuShareAppMessage success');
+                    },
+                    cancel: function() {
+                        // 用户取消分享后执行的回调函数
+                        console.log('onMenuShareAppMessage cancel');
+                    }
+                });
+                wx.onMenuShareTimeline({
+                    title: 'test', // 分享标题
+                    link: 'https://test.weiquaninfo.cn/wxWebMobileTest', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                    imgUrl: 'https://test.weiquaninfo.cn/images/baidu.jpg', // 分享图标
+                    success: () => {
+                        console.log("share success");
+                    },
+                    cancel: () => {
+                        console.log("share cancel");
+                    },
+                    fail: (err) => {
+                        console.log("share failed");
+                    },
+                    complete: () => {
+                        console.log("share complete");
+                    }
+                })
+            });
+            wx.error(function(res) {
+                console.log(res);
+            });
+        }).catch((error) => {
+            console.log(error);
+            alert("setConfig failed");
+        });
+    }
 
-	// 配置jssdk
-	componentDidMount() {
-		this.setConfig().then(signData => {
-			// console.log(signData);
-			wx.config({
-				debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-				appId: 'wxaed97ec85f7517ba', // 必填，公众号的唯一标识
-				timestamp: signData.timestamp, // 必填，生成签名的时间戳
-				nonceStr: signData.noncestr, // 必填，生成签名的随机串
-				signature: signData.signature, // 必填，签名
-				jsApiList: ['chooseImage', 'onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表
-			});
-			// jssdk ready / error
-			wx.ready(function() {
-				console.log('success');
-				wx.onMenuShareAppMessage({
-					title: '123', // 分享标题
-					desc: 'abc', // 分享描述
-					link: 'https://test.weiquaninfo.cn/wxWebMobileTest', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-					imgUrl: 'https://test.weiquaninfo.cn/images/baidu.jpg', // 分享图标
-					type: 'link', // 分享类型,music、video或link，不填默认为link
-					dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-					success: function() {
-						// 用户确认分享后执行的回调函数
-						console.log('onMenuShareAppMessage success');
-					},
-					cancel: function() {
-						// 用户取消分享后执行的回调函数
-						console.log('onMenuShareAppMessage cancel');
-					}
-				});
-				wx.onMenuShareTimeline({
-					title: 'test', // 分享标题
-					link: 'https://test.weiquaninfo.cn/wxWebMobileTest', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-					imgUrl: 'https://test.weiquaninfo.cn/images/baidu.jpg', // 分享图标
-					success: () => {
-						console.log("share success");
-					},
-					cancel: () => {
-						console.log("share cancel");
-					},
-					fail: (err) => {
-						console.log("share failed");
-					},
-					complete: () => {
-						console.log("share complete");
-					}
-				})
-			});
-			wx.error(function(res) {
-				console.log(res);
-			});
-		}).catch((error) => {
-			console.log(error);
-			alert("setConfig failed");
-		});
-	}
+    // 获取签名信息（从服务器端）
+    getSignInfo() {
+        return new Promise((resolve, reject) => {
+            let url = "https://test.weiquaninfo.cn/wxWebMobileTest/getConfigSign";
+            fetch(url).then(res => {
+                let contentType = res.headers.get("Content-Type");
+                if (res.status == 200 && contentType && contentType.includes("application/json")) {
+                    return resolve(res.json());
+                } else {
+                    return reject(new Error(`status:${res.status} contentType:${contentType}`));
+                }
+            })
+        })
+    }
 
-	// 获取签名信息（从服务器端）
-	getSignInfo() {
-		return new Promise((resolve, reject) => {
-			let url = "https://test.weiquaninfo.cn/wxWebMobileTest/getConfigSign";
-			fetch(url).then(res => {
-				let contentType = res.headers.get("Content-Type");
-				if (res.status == 200 && contentType && contentType.includes("application/json")) {
-					return resolve(res.json());
-				} else {
-					return reject(new Error(`status:${res.status} contentType:${contentType}`));
-				}
-			})
-		})
-	}
+    // 
+    onBtnClick(type, e) {
+        switch (type) {
+            case 'checkJsApi':
+                {
+                    wx.checkJsApi({
+                        jsApiList: ['chooseImage', 'onMenuShareTimeline', 'onMenuShareAppMessage'],
+                        success: (res) => {
+                            console.log(res);
+                        },
+                        fail: (err) => {
+                            console.log(err);
+                        }
+                    });
+                    break;
+                }
+            case 'chooseImage':
+                {
+                    wx.chooseImage({
+                        count: 1, // 默认9
+                        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+                        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+                        success: function(res) {
+                            var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                        }
+                    });
+                    break;
+                }
+            case 'navigateBack':
+                {
+                    wx.miniProgram.navigateBack();
+                    break;
+                }
+            case 'onMenuShareAppMessage':
+                {
+                    // console.log('onMenuShareAppMessage begin');
 
-	// 
-	onBtnClick(type, e) {
-		switch (type) {
-			case 'checkJsApi':
-				{
-					wx.checkJsApi({
-						jsApiList: ['chooseImage', 'onMenuShareTimeline', 'onMenuShareAppMessage'],
-						success: (res) => {
-							console.log(res);
-						},
-						fail: (err) => {
-							console.log(err);
-						}
-					});
-					break;
-				}
-			case 'chooseImage':
-				{
-					wx.chooseImage({
-						count: 1, // 默认9
-						sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-						sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-						success: function(res) {
-							var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-						}
-					});
-					break;
-				}
-			case 'navigateBack':
-				{
-					wx.miniProgram.navigateBack();
-					break;
-				}
-			case 'onMenuShareAppMessage':
-				{
-					// console.log('onMenuShareAppMessage begin');
-
-					break;
-				}
-			default:
-				{
-					alert("no button type");
-				}
-		}
-	}
+                    break;
+                }
+            default:
+                {
+                    alert("no button type");
+                }
+        }
+    }
 
 
-	//
-	render() {
-		return (
-			<div>
+    //
+    render() {
+        return (
+            <div>
 				<div className="name">
 					my-test
 				</div>
@@ -179,6 +177,6 @@ export default class App extends React.Component {
 					<WhiteSpace />
 				</WingBlank>
 			</div>
-		);
-	}
+        );
+    }
 }
